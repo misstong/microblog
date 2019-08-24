e6318fc4386b81bbd02329479e8e72d0e3089385
miss tong <1962788480@qq.com>
2019-08-24 19:48:29 +0800

    add translator service but no key

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ from logging.handlers import SMTPHandler,RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask import request
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +20,14 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:

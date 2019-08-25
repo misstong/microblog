9b23e5ac19b034b193cb2514746ba366118db404
miss tong <1962788480@qq.com>
2019-08-25 18:36:15 +0800

    add private message and static badge

--- app/models.py
+++ app/models.py
@@ -17,6 +17,15 @@ followers = db.Table('followers',
     db.Column('follower_id',db.Integer,db.ForeignKey('user.id')),
     db.Column('followed_id',db.Integer,db.ForeignKey('user.id')))
 
+class Message(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    sender_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
 
 class SearchableMixin(object):
     @classmethod
@@ -72,6 +81,14 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers',lazy='dynamic'),lazy='dynamic' 
     )
+
+    message_sent = db.relationship('Message',foreign_keys='Message.sender_id',backref='author',lazy='dynamic')
+    message_received = db.relationship('Message',foreign_keys='Message.recipient_id',backref='recipient',lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900,1,1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from app import app,db
 from flask import render_template
 from datetime import datetime
 import re
-from app.forms import EditProfileForm,PostForm,SearchForm
+from app.forms import EditProfileForm,PostForm,SearchForm,MessageForm
 from flask import flash, redirect, url_for
 from flask_login import current_user,login_user ,logout_user,login_required
-from app.models import User,Post
+from app.models import User,Post,Message
 from flask import request
 from werkzeug.urls import url_parse
 from guess_language import guess_language
@@ -163,3 +163,29 @@ def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user_popup.html',user=user)
 
+@app.route('/send_message/<recipient>',methods=['GET','POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user,recipient=user,body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash('Your message has been sent.')
+        return redirect(url_for('user',username=recipient))
+    return render_template('send_message.html',title="Send Message",form=form,recipient=recipient)
+
+@app.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+    page = request.args.get('page',1,type=int)
+    messages = current_user.message_received.order_by(Message.timestamp.desc()).paginate(
+        page,app.config['POSTS_PER_PAGE'],False)
+    next_url = url_for('messages',page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('messages',page=messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('messages.html',messages=messages.items,next_url=next_url, prev_url=prev_url)
